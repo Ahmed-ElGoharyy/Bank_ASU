a8518b3e8aecb483e0a51f9f57c20c25673a96d3
Ahmed-ElGoharyy <ahmdgohary@gmail.com>
2024-12-03 12:52:11 +0200

    Initial commit

---
+++ src/main/java/com/asu_bank/bank_asu/HelloController.java
@@ -0,0 +1,14 @@
+package com.asu_bank.bank_asu;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
