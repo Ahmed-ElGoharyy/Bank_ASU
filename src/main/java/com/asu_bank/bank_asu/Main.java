7f73431ce8d5d278076ca092701014b0fcabd213
Ahmed-ElGoharyy <ahmdgohary@gmail.com>
2024-12-08 01:39:42 +0200

    Bank class + Login GUI (only admin admin works)

--- src/main/java/com/asu_bank/bank_asu/Main.java
+++ src/main/java/com/asu_bank/bank_asu/Main.java
@@ -1,5 +1,6 @@
 package com.asu_bank.bank_asu;
 
+import  com.asu_bank.bank_asu.Model.Bank;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -13,13 +14,15 @@ public class Main extends Application {
     
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("ASU Bank");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+        Bank bank = Bank.getInstance();
+
         launch();
     }
 }
--- src/main/java/com/asu_bank/bank_asu/Model/Bank.java
+++ src/main/java/com/asu_bank/bank_asu/Model/Bank.java
@@ -1,4 +1,26 @@
 package com.asu_bank.bank_asu.Model;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Bank {
+    protected static Bank instance;
+    protected   List<Client> BankClients;
+    protected   List<Employee> BankEmployees;
+
+
+
+    private Bank() {
+        BankClients = new ArrayList<>();
+        BankEmployees = new ArrayList<>();
+    }
+
+    public static Bank getInstance() {
+        if (instance == null) {
+            instance = new Bank();
+        }
+        return instance;
+    }
 }
