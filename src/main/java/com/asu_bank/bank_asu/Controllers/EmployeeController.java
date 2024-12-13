623adecef524feefcfab5076defa2188f1d98d73
Esraa <esraaemaaad56@gmail.com>
2024-12-13 04:06:15 +0200

    shayfiin

--- src/main/java/com/asu_bank/bank_asu/Controllers/EmployeeController.java
+++ src/main/java/com/asu_bank/bank_asu/Controllers/EmployeeController.java
@@ -1,12 +1,25 @@
 package com.asu_bank.bank_asu.Controllers;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 
-public class EmployeeController {
+import java.net.URL;
+import java.lang.classfile.Label;
+import java.util.ResourceBundle;
 
-
+public class EmployeeController implements Initializable {
     @FXML
+    private Label EmployeeChoicebox;
+    @FXML
+    private ChoiceBox<String> MyEmployeeChoicebox;
+    private String[] employeemethods={"Add account","Add bank account","Edit account","Delete account","Search account","Edit personal informations"};
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        MyEmployeeChoicebox.getItems().addAll(employeemethods);
+
+    }
+@FXML
     private void logoutButtonClicked(ActionEvent event) {
         utility.LogOut(event);
     }
