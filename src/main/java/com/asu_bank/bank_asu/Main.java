ea44ddae176a7d975bed1d69bd5068199b7d6a10
SamaMohamed2 <sama32048@gmail.com>
2024-12-14 01:08:37 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/asu_bank/bank_asu/Main.java
+++ src/main/java/com/asu_bank/bank_asu/Main.java
@@ -1,17 +1,18 @@
 package com.asu_bank.bank_asu;
 
-import  com.asu_bank.bank_asu.Model.Bank;
+import com.asu_bank.bank_asu.Model.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main extends Application {
-
     @Override
-    
+
+
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
@@ -20,14 +21,10 @@ public class Main extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
         Bank bank = Bank.getInstance();
-
-        DataLoader dl=new DataLoader();
+        DataLoader dl = new DataLoader();
         dl.loadData(bank);
-
         launch();
-
     }
 }
