d2051aeceb270267a2bc4456b2dda40e91b14c27
Esraa <esraaemaaad56@gmail.com>
2024-12-14 15:57:54 +0200

    ..

--- src/main/java/com/asu_bank/bank_asu/Controllers/ClientController.java
+++ src/main/java/com/asu_bank/bank_asu/Controllers/ClientController.java
@@ -1,11 +1,21 @@
 package com.asu_bank.bank_asu.Controllers;
 
+import com.asu_bank.bank_asu.Model.Bank;
 import com.asu_bank.bank_asu.Model.Client;
 import com.asu_bank.bank_asu.Model.Employee;
 import com.asu_bank.bank_asu.Model.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class ClientController {
 
@@ -40,3 +50,5 @@ public class ClientController {
         utility.LogOut(event);
     }
 }
+
+
